3b5ac40c92b88d259cd7cc848946ffa6b099367f
Abhilash Pal <abhilashpal8@gmail.com>
2018-04-07 00:18:15 +0530

    Add files via upload

--- Xor.py
+++ Xor.py
@@ -19,6 +19,16 @@ for j in range(100000):
 	l1 = nonlin(np.dot(l0,w0))
 	l2 = nonlin(np.dot(l1,w1))
 
-	l2_error = y-l2
-	if(j%10000)==0:
-		print("Err:"+str(np.mean(np.abs)))
+	l2_error = Y-l2
+	if(j%20000)==0:
+		print("Err:"+str(np.mean(np.abs(l2_error))))
+
+	l2_delta = l2_error*nonlin(l2,deriv=True)
+	l1_error = l2_delta.dot(w1.T)
+	l1_delta = l1_error*nonlin(l1,deriv=True)
+
+	w1 += l1.T.dot(l2_delta)
+	w0 += l0.T.dot(l1_delta)
+
+print("After Training:")
+print(l2)
